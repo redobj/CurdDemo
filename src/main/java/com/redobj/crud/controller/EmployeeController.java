c4c09f8b99495a294473b15e7eb3dd230804fd8f
JackZou <redobjwork@outlook.com>
2017-12-26 02:15:01 +0800

    12.26-1 ajax通用改造 后端完成 返回json
    改造Ajax查询，提高系统扩展性
    1、index.jsp页面直接发送ajax请求进行员工分页数据的查询
    2、服务器将查出的数据，以json字符串的形式返回给浏览器
    3、浏览器收到js字符串。可以用js对json进行解析，使用js通过dom增删改变页面
    4、返回json。实现客户端的无关性。
    
    pom.xml
    * 增加jackson databind依赖
    
    EmployeeController
    * 增加getEmpsWithJson方法，处理请求返回json字符串
    
    新增通用返回类 bean.Msg
    * 设置状态码，通知，及包装数据
    * 为包装数据设置一个可以链式操作的add方法

--- src/main/java/com/redobj/crud/controller/EmployeeController.java
+++ src/main/java/com/redobj/crud/controller/EmployeeController.java
@@ -7,10 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.redobj.crud.bean.Employee;
+import com.redobj.crud.bean.Msg;
 import com.redobj.crud.service.EmployeeService;
 
 @Controller
@@ -23,7 +25,7 @@ public class EmployeeController {
 	 * 查询员工数据
 	 * @return
 	 */
-	@RequestMapping("/emps")
+	//@RequestMapping("/emps")
 	public String listEmps(@RequestParam(value="pn",defaultValue="1")Integer pn,Model model) {
 //		非分页
 //		List<Employee> emps = employeeService.getAll();
@@ -37,5 +39,22 @@ public class EmployeeController {
 		PageInfo<Employee> page = new PageInfo<Employee>(emps,5);
 		model.addAttribute("pageInfo",page);
 		return "list";
+	}
+	
+	/**
+	 * 导入Jackson包，保证ResponseBody正常工作返回json字符串
+	 * @param pn
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping("/emps")
+	@ResponseBody
+	public Msg getEmpsWithJson(@RequestParam(value="pn",defaultValue="1")Integer pn,Model model) {
+		PageHelper.startPage(pn,10);
+//		startPage后面紧跟的查询就是分页查询 
+		List<Employee> emps = employeeService.getAll();
+//		使用pageInfo包装查询后的结果，只需要将pageInfo交给页面
+		PageInfo<Employee> page = new PageInfo<Employee>(emps,5);
+		return Msg.success().add("pageInfo", page);
 	}
 }
